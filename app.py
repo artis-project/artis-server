37e7ae3d0c628ce106cf25b29b23f20aa05b969d
flyingbeat <69470817+flyingbeat@users.noreply.github.com>
2023-05-15 20:50:35 +0200

    implementing api methods

--- app.py
+++ app.py
@@ -1,37 +1,64 @@
-# Copyright 2021 Google LLC
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
+import binascii
+import os
 import signal
 import sys
 from types import FrameType
 
-from flask import Flask
+from dotenv import load_dotenv
+from flask import Flask, g, request
 
+from src.models.Artwork import Artwork
+from src.smartcontracts.DefaultArtwork import DefaultArtwork
+from utils.authentication import verify_signature
+from utils.error_handlers import register_error_handlers
 from utils.logging import logger
 
+### SETUP ###
+load_dotenv()
 app = Flask(__name__)
+app.app_context()
+sc = DefaultArtwork(
+    signing_private_key=os.environ.get("DEFAULT_PRIVATE_KEY"),
+    http_provider_url=os.environ.get("HTTP_PROVIDER_URL"),
+)
 
 
+### MIDDLEWARE ###
+@app.before_request
+def authenticate():
+    signature = request.headers.get("signature")
+    did = request.headers.get("did")
+
+    sender = verify_signature(did=did, signature=signature, smartcontract=sc)
+    g.sender = binascii.unhexlify(sender[2:])
+
+    logger.info(sender=sender)
+
+
+### ROUTES ###
 @app.route("/")
 def hello() -> str:
-    # Use basic logging with custom fields
-    logger.info(logField="custom-entry", arbitraryField="custom-entry")
+    return "Hello from Artis-Project!"
 
-    # https://cloud.google.com/run/docs/logging#correlate-logs
-    logger.info("Child logger with trace Id.")
 
-    return "Hello, World! This works!"
+@app.get("/artworks/<int:artwork_id>")
+def get(artwork_id: str) -> str:
+    return sc.getArtworkData(artwork_id, g.sender).dump()
+
+
+@app.patch("/artworks/<int:artwork_id>")
+def update(artwork_id: int) -> str:
+    newArtworkData = Artwork.load(request.get_json() | {"id": artwork_id})
+    return sc.updateArtworkData(newArtworkData, g.sender).dump()
+
+
+@app.post("/artworks")
+def mint() -> str:
+    return {"tokenId": sc.safeMint(to=g.sender)}
+
+
+### HANDLERS ###
+register_error_handlers(app)
 
 
 def shutdown_handler(signal_int: int, frame: FrameType) -> None:
