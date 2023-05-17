d54bd2e6d602d801971bfb9885e48bf2b0714335
flyingbeat <69470817+flyingbeat@users.noreply.github.com>
2023-05-17 13:46:25 +0200

    adding generic werkzeug error handler

--- utils/error_handlers.py
+++ utils/error_handlers.py
@@ -1,15 +1,11 @@
 from flask import Flask
 from marshmallow import ValidationError
 from web3.exceptions import ContractLogicError
-from werkzeug.exceptions import Unauthorized
+from werkzeug.exceptions import HTTPException, MethodNotAllowed, NotFound, Unauthorized
 
 
 def validation_error(error: ValidationError):
     return {"error": error.__class__.__name__, "messages": error.messages}, 400
-
-
-def unauthorized_error(error: Unauthorized):
-    return {"error": error.__class__.__name__, "messages": error.description}, 401
 
 
 def contract_logic_error(error: ContractLogicError):
@@ -19,7 +15,18 @@ def contract_logic_error(error: ContractLogicError):
         return {"error": error.__class__.__name__, "messages": error.args}, 403
 
 
+def werkzeug_errors(error: HTTPException):
+    return {
+        "error": error.__class__.__name__,
+        "messages": error.description,
+    }, int(error.code)
+
+
 def register_error_handlers(app: Flask):
+    ### Werkzeug errors ###
+    app.register_error_handler(NotFound, werkzeug_errors)
+    app.register_error_handler(Unauthorized, werkzeug_errors)
+    app.register_error_handler(MethodNotAllowed, werkzeug_errors)
+    ### other errors ###
     app.register_error_handler(ValidationError, validation_error)
-    app.register_error_handler(Unauthorized, unauthorized_error)
     app.register_error_handler(ContractLogicError, contract_logic_error)
